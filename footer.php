e7cdb6e65474028f899bbdf271e36ea0eddd978e
Verbal-Kint <vincent.gerard70@gmail.com>
2017-02-24 11:22:47 +0100

    footer de register_controller.php
    footer de register_controller.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+</div><br/><br/><br/>
+<form action="" method="POST">
+	<div class="fomr-group">
+		<label for="">Pseudo</label>
+		<input type="text" name="username" class="form-control"/>
+	</div>
+
+	<div class="fomr-group">
+		<label for="">eMail</label>
+		<input type="text" name="email" class="form-control"/>
+	</div>
+
+	<div class="fomr-group">
+		<label for="">Mot de passe</label>
+		<input type="password" name="password" class="form-control"/>
+	</div>
+
+	<div class="fomr-group">
+		<label for="">Confirmez votre mot de passe</label>
+		<input type="password" name="password_confirm" class="form-control"/>
+	</div>
+
+	<button type="submit" class="btn btn-primaryy">M'inscrire</button>
+
+</form>
+</body>
+</html>
