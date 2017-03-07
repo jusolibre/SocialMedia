6bd68fc1507b049dc0e0ab817076b9996c857f09
jusolibre <julien.s@codeur.online>
2017-03-07 21:12:18 +0100

    add some friends fix

--- controller/friends_controller.php
+++ controller/friends_controller.php
@@ -23,8 +23,28 @@ class friends {
 
     $db = new Database('socialmedia');
     $bdd = $db->getter();
-    
-   if (isset($tab[0]) && $tab[0] == 'add')
+
+
+    $display = "";
+
+
+    if ((isset($tab[0]) && $tab[0] == 'search' &&
+    isset($tab[1]) && $tab[1] != 'add' &&
+    $tab[1] != 'remove' && $tab[1] != 'search') ||
+    (isset($tab[2]) && $tab[2] == 'search' &&
+    isset($tab[3]) && $tab[3] != 'add' &&
+    $tab[3] != 'remove' && $tab[3] != 'search')) {
+        $search = $tab[3];
+    }
+    if (isset($_POST) && isset($_POST['search'])) {
+        $search = $_POST['search'];
+    }
+
+    if (isset($search)) {
+        $display = $display . "<header>Result for search : " . $search . "</header>";
+    }
+
+   if (isset($tab[0]) && $tab[0] == 'add' && $tab[1] != 'remove' && $tab[1] != 'add' && $tab[1] != 'search')
  {
   $add = htmlspecialchars(($tab[1]));
   $req = $bdd->query("SELECT * FROM amis WHERE utilisateur1 = '" . $tab[1] . "' AND utilisateur2 = '" . $_SESSION['user']->id . "'");
@@ -34,22 +54,30 @@ class friends {
  {
    $req = $bdd->query("INSERT INTO amis VALUES ('" . $tab[1] . "', '" . $_SESSION['user']->id . "')");
  }}
-  else if (isset($tab[0]) && $tab[0] == 'remove')
-  {   $remove = htmlspecialchars($tab[1]);
-   $req = $bdd->query("DELETE FROM amis WHERE utilisateur1 = '" . $tab[1] . "' AND utilisateur2 = '" . $_SESSION['user']->id . "'");
+  else if (isset($tab[0]) && $tab[0] == 'remove' && $tab[1] != 'remove' && $tab[1] != 'add' && $tab[1] != 'search')
+  {
+      $remove = htmlspecialchars($tab[1]);
+      $req = $bdd->query("DELETE FROM amis WHERE utilisateur1 = '" . $tab[1] . "' AND utilisateur2 = '" . $_SESSION['user']->id . "'");
   }
     
-    $req = $bdd->query("SELECT prenom, nom, id FROM utilisateur ORDER BY id");
+   if (isset($search)) {
+       $req = $bdd->query("SELECT prenom, nom, id FROM utilisateur WHERE nom LIKE '%" . $search . "%' OR prenom LIKE '%" . $search . "%' OR description LIKE '%" . $search . "%' OR email LIKE '%" . $search . "%' OR pays LIKE '%" . $search . "%' OR ville LIKE '%" . $search . "%' ORDER BY id");
+   } else {
+       $req = $bdd->query("SELECT prenom, nom, id FROM utilisateur ORDER BY id");
+   }
     $donnees = $req->fetchAll(PDO::FETCH_ASSOC);
 
-    $display = "";
+    if (isset($search))
+        $post = "/search/" . $search . "/";
+    else
+        $post = "";
     
    foreach ($donnees as $key)
  {
      if ($key['prenom'] == $_SESSION['user']->prenom)
          {
-          continue;
-          }
+             continue;
+         }
     $display = $display . "<li><a href='". WEBROOT . "profil/display&" . $key['id'] . "'>" . $key['prenom'] . " " . $key['nom'] . "</a>";
       $follow = "Suivre";
 
@@ -61,11 +89,11 @@ class friends {
 
     if (!$reponse1)
      {
-       $display = $display . " <a href='". WEBROOT ."friends/friend&add/" . $key['id'] . "'><button class='button'>follow</button></a></li>";
+       $display = $display . " <a href='". WEBROOT ."friends/index&add/" . $key['id'] . $post . "'><button class='button'>follow</button></a></li>";
     }
      else
     {        
-      $display = $display . " <a href='". WEBROOT ."friends/friend&remove/" . $key['id'] . "'><button class='button2'>Suprimmer</button></a></li>";
+      $display = $display . " <a href='". WEBROOT ."friends/index&remove/" . $key['id'] . $post . "'><button class='button2'>Suprimmer</button></a></li>";
     }
    }
    $this->renderPage($display);
